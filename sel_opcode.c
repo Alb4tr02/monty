97c307763709f0543f343eaf80acdfd02a74f09f
Jimmer942 <942@holbertonschool.com>
2019-09-26 10:29:54 -0400

    FAILE

--- sel_opcode.c
+++ sel_opcode.c
@@ -12,7 +12,7 @@ int sel_opcode(stack_t **stack, unsigned int line)
 		{ "push", push }, { "pall", pall}, { "pop", pop },
 		{ "add", add }, { "nop", nop }, { "sub", sub },
 		{ "pint", pint }, { "swap", swap },
-		{ "mul", mul }, { "div", div_m }, { "pchar", pchar },
+		{ "mul", mul }, { "div", div_m }, { "pchar", _pchar},
 		{ "mod", mod_m }, { "\n", nop }, { " ", nop },
 		{ "/t", nop }, { "pstr", pstr_t }, { NULL, NULL }
 	};
