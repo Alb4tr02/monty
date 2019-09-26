0432bfe50e07e4ce3989f4e13aa43ed548ab8c69
Alb4Tr02 <834@holbertonschool.com>
2019-09-26 10:15:23 -0400

    functions update

---
+++ div_m.c
@@ -0,0 +1,5 @@
+#include "monty.h"
+void div_m(stack_t **stack, unsigned int line_number)
+{
+	return;
+}
---
+++ pstr_t.c
@@ -0,0 +1,5 @@
+#include "monty.h"
+void pstr_t(stack_t **stack, unsigned int line_number)
+{
+	return;
+}
