10efcaeabde9681e6aff06c6e756d277a5decefc
Jimmer942 <942@holbertonschool.com>
2019-09-26 23:06:12 -0400

    rotr.c

---
+++ rotr.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+/**
+ * rotr -rotates the stack to the top.
+ * @stack: a double linked list
+ * @line_number: number of line in code to print error message.
+ */
+void rotr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *p = NULL;
+	(void)line_number;
+
+	if (*stack == NULL)
+		return;
+	do {
+		p = (*stack)->prev;
+		(*stack)->prev = (*stack)->next;
+		(*stack)->next = p;
+		if (p != NULL)
+			*stack = p;
+	} while (p != NULL);
+}
