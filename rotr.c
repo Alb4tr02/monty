423c19f1b1c3a4c22ecf9e37ba5508906afc2183
Jimmer942 <942@holbertonschool.com>
2019-09-27 08:25:06 -0400

    rotr.c

--- rotr.c
+++ rotr.c
@@ -6,18 +6,28 @@
  */
 void rotr(stack_t **stack, unsigned int line_number)
 {
-	stack_t *p = NULL;
+	stack_t *p = NULL, *new = NULL, *aux = NULL;
+	int i = 0;
 	(void)line_number;
 
 	if (*stack == NULL)
 		return;
 	if ((*stack)->prev == NULL)
 		return;
-	do {
-		p = (*stack)->prev;
-		(*stack)->prev = (*stack)->next;
-		(*stack)->next = p;
-		if (p != NULL)
-			*stack = p;
-	} while (p != NULL);
+	p = *stack;
+	while (p->prev != NULL)
+		p = p->prev;
+	i = p->n;
+	aux = p->next;
+	free(p);
+	aux->prev = NULL;
+	new = (stack_t *)malloc(sizeof(stack_t));
+	if (new == NULL)
+		error_malloc(stack, NULL, NULL, NULL);
+	new->n = i;
+	new->next = NULL;
+	new->prev = *stack;
+	p = *stack;
+	p->next = new;
+	*stack = new;
 }
